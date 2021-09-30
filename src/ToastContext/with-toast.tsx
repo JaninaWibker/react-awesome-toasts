7613c56b6959181c312d3ec8dfe652628c4d6e28
Jannik Wibker <jannikwibker@gmail.com>
2021-09-30 13:49:22 +0200

    added tsdoc comments to types

--- src/ToastContext/with-toast.tsx
+++ src/ToastContext/with-toast.tsx
@@ -1,24 +1,45 @@
 import * as React from 'react'
 import ToastConsumer from './toast-consumer'
 
-export type IReactComponent<P = any> =
+/**
+ * Type for metaclasses extending a certain type T
+ * 
+ * Values of this type are classes that extend T
+ */
+declare type Class<T = any> = new (...args: any[]) => T
+
+type IReactComponent<P = any> =
     | React.ClassicComponentClass<P>
     | React.ComponentClass<P>
     | React.FunctionComponent<P>
     | React.ForwardRefExoticComponent<P>
 
-export declare type Class<T = any> = new (...args: any[]) => T
+/**
+ * Extract props from react component
+ */
+type getProps<C> = C extends React.Component<infer T> ? T : never
 
-export type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>
-
+/**
+ * Decorator for injecting a toast context into the props of a react component using a ToastConsumer.
+ *
+ * @remarks The class being decorated needs to have context: ToastContext in its prop types
+ * to safely use this (from a type system perspective). Class decorators however cannot change
+ * the outwards appearance of a class without changing how the actual class looks from the
+ * inside as well. This means that this decorator cannot remove the context-field from the props.
+ * To do this removeContextProp can be used, for this to work the class cannot be created and
+ * exported at the same time as it needs to be wrapped in removeContextProp.
+ * With this limitation you could also just use withToast which basically does all of this in
+ * one step without needing the decorator at all.
+ *
+ * Another option is to mark context as optional, but this requires always narrowing the type
+ * to a non-undefined value.
+ */
 const withToastContext = <C extends IReactComponent<any>>(Component: C): C => (
   (props: any) => (
     <ToastConsumer>
       {context => <Component {...props} dispatch={context} />}
     </ToastConsumer>
   )) as any as C
-
-type getProps<C> = C extends React.Component<infer T> ? T : never
 
 /**
  * Removes the context-field from the props of a react component by wrapping it in a
@@ -31,6 +52,9 @@ const removeContextProp = <C extends React.Component<any>>(Component: Class<C>) 
   (props: Omit<getProps<C>, 'dispatch'>) =>
     (<Component {...props} />)
 
+/**
+ * Inject a toast context into a given component and omit the toast context from its list of properties returning a new component
+ */
 const withToast = (Component: any) => removeContextProp(withToastContext(Component))
 
 export default withToast
