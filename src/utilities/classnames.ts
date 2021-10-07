81ddb46f2ec369f4264b1b5886752bbe5d3843d3
Jannik Wibker <jannikwibker@gmail.com>
2021-10-07 16:39:38 +0200

    added tsdoc to classnames.ts

--- src/utilities/classnames.ts
+++ src/utilities/classnames.ts
@@ -1,3 +1,14 @@
+/**
+ * given an array of strings (which might be undefined) filter out all falsy values and at last join them together with spaces
+ * 
+ * @example
+ * ```
+ * // assuming this.props.classname can be undefined, but is 'first-classname' in this case
+ * // assuming some_condition evaluates to true, some_other_condition evaluates to false
+ * classnames(this.props.classname, some_condition && 'some-classname', some_other_condition && 'last-classname')
+ * // => 'first-classname some-classname'
+ * ```
+ */
 const classnames = (...classnames: (string | undefined)[]) => classnames
   .filter(Boolean)
   .join(' ')
