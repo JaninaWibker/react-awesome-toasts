d800f69d6c28ea67d130eeb2b80beb5c5295442f
Jannik Wibker <jannikwibker@gmail.com>
2021-10-07 15:59:57 +0200

    added tsdoc explanations to Props type

--- src/types/Toast.ts
+++ src/types/Toast.ts
@@ -1,15 +1,39 @@
 import type { MouseEvent } from 'react'
 
 export type Props = {
+  /**
+   * A slight variation of the display style of the toast.
+   * This adds a css classname to the element, but is however restricted to values specified in the `toast.css`-file of this project.
+   */
+   variant?: string,
+   /**
+    * The text of the action button.
+    */
+   action_text?: string,
+   /**
+    * The action to be taken when clicking the action button.
+    */
+   on_action_click?: (id: string, event: MouseEvent<HTMLButtonElement>) => any,
+} & ({
+  /**
+   * Text to display in the toast, must be either a string or an array of strings or JSX elements (mixing both is explicitely allowed)
+   * If an array is provided the `aria_label`-property must be specified.
+   */
   text: string,
+  /**
+   * ARIA label for the toast element, this allows for better screen reader accessibility.
+   * Defaults to the `text`-property, unless the text property is an array; then this property is strictly required.
+   */
   aria_label?: string,
-  variant?: string,
-  action_text?: string,
-  on_action_click?: (id: string, event: MouseEvent<HTMLButtonElement>) => any,
 } | {
+  /**
+   * Text to display in the toast, must be either a string or an array of strings or JSX elements (mixing both is explicitely allowed)
+   * If an array is provided the `aria_label`-property must be specified.
+   */
   text: (string | JSX.Element)[],
+  /**
+   * ARIA label for the toast element, this allows for better screen reader accessibility.
+   * Defaults to the `text`-property, unless the text property is an array; then this property is strictly required.
+   */
   aria_label: string,
-  variant?: string
-  action_text?: string,
-  on_action_click?: (id: string, event: MouseEvent<HTMLButtonElement>) => any,
-}
+})
